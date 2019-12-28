7c6221a02c2c951d292669384111143e4dd4c6f9
NileshGhadge05 <ghadgenilesh60@gmail.com>
2019-12-28 19:38:28 +0530

    3 files modified

--- src/app/products.ts
+++ src/app/products.ts
@@ -13,6 +13,11 @@ export const products = [
     name: 'Phone Standard',
     price: 299,
     description: ''
+  },
+  {
+    name: 'Android Pie Smartphone',
+    price: 200,
+    description: ''
   }
 ];
 
